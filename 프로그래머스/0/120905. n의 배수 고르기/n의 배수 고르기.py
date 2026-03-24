ae39224fa04de6e8d2e8c9e1a15f221e623a0178
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 22:53:09 +0900

    정답 추가

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,6 +1,14 @@
+# 첫 번째 답
+# 기본형
 def solution(n, numlist):
     rst = []
     for i in numlist:
         if i % n == 0:
             rst.append(i)
     return rst
+
+# 두 번째 답 (참고)
+# 리스트 컴프리헨션 -> 내부적으로 최적화되어 있어 약간 빠름
+def solution(n, numlist):
+    ans = [i for i in numlist if i % n == 0]
+    return ans
