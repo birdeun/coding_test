451f9c94e980d04ebc0955b9442383cefb0bf3c5
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 22:29:50 +0900

    정답 추가

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -13,13 +13,10 @@ def solution(n):
 # 1000번이면 연산 끝남
 # 약수는 쌍으로 존재하기 때문에, √n 기준으로 대칭임
 def solution(n):
-    answer = 0
+    sum = 0
     for i in range(1, int(n ** 0.5) + 1):
         if n % i == 0:
-            answer += 2
-
-            # 예외: 제곱수인 경우
+            sum += 2
             if i * i == n:
-                answer -= 1
-
-    return answer
+                sum -= 1
+    return sum
