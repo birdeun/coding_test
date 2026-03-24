9d67647db014f81a1f085a21ff14945da28645cf
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 19:23:18 +0900

    정답 추가
    Added multiple implementations of the solution function to calculate similarity between two arrays.

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,21 @@
+# 첫 번째 답
+# 인덱스 접근 필요하다는 단점이 있음
 def solution(s1, s2):
     sum = 0
     for i in range(len(s1)):
         if s1[i] in s2:
             sum += 1
     return sum
+
+# 두 번째 답
+def solution(s1, s2):
+    sum = 0
+    for i in s1:
+        if i in s2:
+            sum += 1
+    return sum
+
+# 세 번째 답 (참고)
+# set 사용해서 유니크 개수 구하는 방식으로 나타낼 수 있음
+def solution(s1, s2):
+    return (len(set(s1)&set(s2)))
