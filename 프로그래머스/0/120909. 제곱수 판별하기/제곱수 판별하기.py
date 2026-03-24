c10e0e00c6b6ee729885f218f2bdfe1cfc407bfc
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 23:46:15 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -8,3 +8,8 @@ def solution(n):
 # 두 번째 답 (간략히)
 def solution(n):
     return 1 if (n ** 0.5) % int(n ** 0.5) == 0 else 2
+
+# 세 번째 답 (참고)
+# 그냥 1로 나누면 될 것을 ...
+def solution(n):
+    return 1 if (n ** 0.5) % 1 == 0 else 2
