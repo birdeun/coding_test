c236ab01b482614b30705eb7030bb4bed4a10735
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 22:26:52 +0900

    정답 추가

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,25 @@
+# 첫 번째 답
+# 완전 탐색 방식 -> 시간복잡도 O(N) 
+# n <= 1,000,000 이기 때문에 100만 번 반복함
 def solution(n):
     sum = 0
     for i in range(1, n+1):
         if n % i == 0:
             sum += 1
     return sum
+
+# 두 번째 답 (참고)
+# 최적화 -> 시간복잡도 O(√n)
+# 1000번이면 연산 끝남
+# 약수는 쌍으로 존재하기 때문에, √n 기준으로 대칭임
+def solution(n):
+    answer = 0
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            answer += 2
+
+            # 예외: 제곱수인 경우
+            if i * i == n:
+                answer -= 1
+
+    return answer
