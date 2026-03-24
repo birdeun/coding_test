61dd11d157ecb403e36857a7384bd07568930d12
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-24 19:25:59 +0900

    정답 추가

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -3,5 +3,6 @@ def solution(slice, n):
     return (n // slice) if (n % slice == 0) else (n // slice + 1)
 
 # 두 번째 답
+# 나누어떨어지는 수가 없도록 n-1로 방지하기
 def solution(slice, n):
     return (n - 1) // slice + 1
