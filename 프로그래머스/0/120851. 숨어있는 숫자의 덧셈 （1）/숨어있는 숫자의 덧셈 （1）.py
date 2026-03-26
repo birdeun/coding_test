a74cfd5b7209752c1f2d3d08dae72f08e68e9857
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-03-26 18:01:25 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,6 +1,17 @@
+# 첫 번째 답
+# 아스키 코드 활용
 def solution(my_string):
     sum = 0
     for k in my_string:
         if '0' <= k <= '9':
             sum += int(k)
+    return sum
+
+# 두 번째 답
+def solution(my_string):
+    sum = 0
+    num = '1234567890'
+    for i in range(len(my_string)):
+        if my_string[i] in num:
+            sum += int(my_string[i])
     return sum
