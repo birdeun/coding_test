dc6b8212cc35f587c462112e0cbb90d31ecc4e4f
Seeun An <126042651+birdeun@users.noreply.github.com>
2026-04-06 00:38:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,3 +1,4 @@
+# sol.1
 def solution(s):
     word = list(s)
     idx = 0
@@ -13,3 +14,21 @@ def solution(s):
             idx += 1
     return ''.join(word)
             
+# sol.2 using ASCII
+def solution(s):
+    word = list(s)
+    idx = 0
+
+    for i in range(len(word)):
+        if word[i] == ' ':
+            idx = 0
+        else:
+            if idx % 2 == 0:
+                if 'a' <= word[i] <= 'z':
+                    word[i] = chr(ord(word[i]) - 32)
+            else:
+                if 'A' <= word[i] <= 'Z':
+                    word[i] = chr(ord(word[i]) + 32)
+            idx += 1
+
+    return ''.join(word)
